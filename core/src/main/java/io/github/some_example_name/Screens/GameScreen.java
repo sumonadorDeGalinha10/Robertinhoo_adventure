15bff72fc76fbcb74a715c54c96414d0e4e1e3ac
Wanderson Guedes <wg21249@gmail.com>
2025-06-19 22:29:26 -0300

    newHud_Robertinhooo

--- core/src/main/java/io/github/some_example_name/Screens/GameScreen.java
+++ core/src/main/java/io/github/some_example_name/Screens/GameScreen.java
@@ -4,15 +4,12 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import io.github.some_example_name.Mapa;
-import io.github.some_example_name.Entities.Itens.Weapon.Pistol.Pistol;
 import io.github.some_example_name.Entities.Player.Robertinhoo;
+import io.github.some_example_name.Interface.RobertinhoFaceHUD;
 import io.github.some_example_name.Interface.WeaponHUD;
 import io.github.some_example_name.MapRenderer;
-
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-
 
 public class GameScreen extends CatScreen {
 
@@ -22,6 +19,7 @@ public class GameScreen extends CatScreen {
     private WeaponHUD weaponHUD;
     private SpriteBatch hudBatch;
     private OrthographicCamera hudCamera;
+    private RobertinhoFaceHUD robertinhoFaceHUD;
 
     public GameScreen(Game game) {
         super(game);
@@ -32,59 +30,69 @@ public class GameScreen extends CatScreen {
         mapa = new Mapa();
         robertinhoo = mapa.robertinhoo;
         renderer = new MapRenderer(mapa);
+        
         hudBatch = new SpriteBatch();
-        weaponHUD = new WeaponHUD(hudBatch, robertinhoo);
         hudCamera = new OrthographicCamera();
         hudCamera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        hudBatch = new SpriteBatch();
         hudBatch.setProjectionMatrix(hudCamera.combined);
-        weaponHUD = new WeaponHUD(hudBatch, robertinhoo);
+        
+        weaponHUD = new WeaponHUD(robertinhoo);
+        float width = Gdx.graphics.getWidth();
+        float height = Gdx.graphics.getHeight();
+        
+        // Passa as dimensões para o FaceHUD
+        robertinhoFaceHUD = new RobertinhoFaceHUD(370, width, height);
+        
         robertinhoo.setMapRenderer(renderer);
+        weaponHUD.setBatch(hudBatch);
 
-  
         System.out.println("MapRenderer configurado no Robertinhoo.");
     }
 
     @Override
     public void render(float delta) {
-        // Limita o delta para evitar variações extremas de tempo de quadro
         delta = Math.min(0.06f, Gdx.graphics.getDeltaTime());
 
-        // Limpa a tela
         Gdx.gl.glClearColor(0.1f, 0.1f, 0.1f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        // Atualiza a posição e animação do jogador
-      
         robertinhoo.update(delta);
-
         mapa.update(delta);
-   
 
-        // Renderiza o mapa e o jogador
         renderer.render(delta, robertinhoo);
-        hudBatch.setProjectionMatrix(hudCamera.combined);
-        weaponHUD.update(delta);
-        weaponHUD.draw();
         
+        weaponHUD.update(delta);
+        robertinhoFaceHUD.update(delta);
+        
+        hudBatch.begin();
+        weaponHUD.draw();
+        robertinhoFaceHUD.draw(hudBatch); 
+        hudBatch.end();
     }
 
     @Override
     public void resize(int width, int height) {
-      
-        
         renderer.resize(width, height);
-        weaponHUD.resize(width, height);
         
-
+        if (weaponHUD != null) {
+            weaponHUD.resize(width, height);
+        }
+        
         hudCamera.setToOrtho(false, width, height);
         hudCamera.update();
+        hudBatch.setProjectionMatrix(hudCamera.combined);
+        robertinhoFaceHUD.updateScreenSize(width, height);
     }
 
     @Override
     public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+     
         if (renderer != null) {
-            System.out.println("Destruindo MapRenderer...");
             renderer.dispose();
         }
         if (weaponHUD != null) {
@@ -92,6 +100,10 @@ public class GameScreen extends CatScreen {
         }
         if (hudBatch != null) {
             hudBatch.dispose();
+        }
+
+        if (robertinhoFaceHUD != null) {
+            robertinhoFaceHUD.dispose();
         }
     }
 }
