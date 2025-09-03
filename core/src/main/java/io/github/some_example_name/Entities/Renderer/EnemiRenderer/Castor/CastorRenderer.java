f568de83fddb3264d40c72a0268e4731d9797a81
Wanderson Guedes <wg21249@gmail.com>
2025-09-03 13:18:43 -0300

    new castor animations

--- core/src/main/java/io/github/some_example_name/Entities/Renderer/EnemiRenderer/Castor/CastorRenderer.java
+++ core/src/main/java/io/github/some_example_name/Entities/Renderer/EnemiRenderer/Castor/CastorRenderer.java
@@ -10,36 +10,97 @@ import com.badlogic.gdx.Gdx;
 
 public class CastorRenderer {
     private Animation<TextureRegion> idleAnimation;
+    private Animation<TextureRegion> runLeftAnimation;
+    private Animation<TextureRegion> runDownAnimation;
+    private Animation<TextureRegion> runUpAnimation;
     private float stateTime;
     private static final float FRAME_DURATION = 0.1f;
+    private Direction currentDirection = Direction.DOWN;
+    private boolean isMoving = false;
+
+    private enum Direction {
+        LEFT, RIGHT, DOWN, UP
+    }
 
     public CastorRenderer() {
-        Texture castorSheet = new Texture(Gdx.files.internal("enemies/castor/idle-Sheet.png"));
-        TextureRegion[][] tmp = TextureRegion.split(castorSheet, 
-            castorSheet.getWidth() / 8, 
-            castorSheet.getHeight());
+        // Carrega a spritesheet do idle
+        Texture idleSheet = new Texture(Gdx.files.internal("enemies/castor/idle-Sheet.png"));
+        TextureRegion[][] idleFramesGrid = TextureRegion.split(idleSheet, 
+            idleSheet.getWidth() / 8, 
+            idleSheet.getHeight());
+        idleAnimation = new Animation<>(FRAME_DURATION, idleFramesGrid[0]);
         
-        TextureRegion[] idleFrames = new TextureRegion[8];
-        for (int i = 0; i < 8; i++) {
-            idleFrames[i] = tmp[0][i];
-        }
+        // Carrega a spritesheet do run
+        Texture runSheet = new Texture(Gdx.files.internal("enemies/castor/RUN-Sheet.png"));
+        int runFrameWidth = runSheet.getWidth() / 5;
+        int runFrameHeight = runSheet.getHeight() / 3;
+        TextureRegion[][] runFrames = TextureRegion.split(runSheet, runFrameWidth, runFrameHeight);
         
-        idleAnimation = new Animation<>(FRAME_DURATION, idleFrames);
+        runLeftAnimation = new Animation<>(FRAME_DURATION, runFrames[0]);
+        runDownAnimation = new Animation<>(FRAME_DURATION, runFrames[1]);
+        runUpAnimation = new Animation<>(FRAME_DURATION, runFrames[2]);
+        
         stateTime = 0f;
     }
 
+    private Direction getDirection(Vector2 velocity) {
+        float absVelX = Math.abs(velocity.x);
+        float absVelY = Math.abs(velocity.y);
+        
+        if (absVelX > absVelY) {
+            return velocity.x > 0 ? Direction.RIGHT : Direction.LEFT;
+        } else {
+            return velocity.y > 0 ? Direction.UP : Direction.DOWN;
+        }
+    }
+
     public void render(SpriteBatch batch, Castor castor, float offsetX, float offsetY, float delta) {
+        Vector2 velocity = castor.getLinearVelocity();
+        isMoving = velocity.len() > 0.1f;
+        
+        if (isMoving) {
+            currentDirection = getDirection(velocity);
+        }
+        
         stateTime += delta;
-        TextureRegion currentFrame = idleAnimation.getKeyFrame(stateTime, true);
+        TextureRegion currentFrame;
+        boolean flipX = false;
+        
+        if (isMoving) {
+            switch (currentDirection) {
+                case LEFT:
+                    currentFrame = runLeftAnimation.getKeyFrame(stateTime, true);
+                    break;
+                case RIGHT:
+                    currentFrame = runLeftAnimation.getKeyFrame(stateTime, true);
+                    flipX = true;
+                    break;
+                case DOWN:
+                    currentFrame = runDownAnimation.getKeyFrame(stateTime, true);
+                    break;
+                case UP:
+                    currentFrame = runUpAnimation.getKeyFrame(stateTime, true);
+                    break;
+                default:
+                    currentFrame = idleAnimation.getKeyFrame(stateTime, true);
+            }
+        } else {
+            currentFrame = idleAnimation.getKeyFrame(stateTime, true);
+        }
         
         Vector2 position = castor.getPosition();
-        float x = offsetX + position.x * 16 - 8; // Ajuste para centralizar
-        float y = offsetY + position.y * 16 - 8; // Ajuste para centralizar
+        float x = offsetX + position.x * 16 - 8;
+        float y = offsetY + position.y * 16 - 8;
         
-        batch.draw(currentFrame, x, y, 16, 16);
+        if (flipX) {
+            batch.draw(currentFrame, x + 16, y, -16, 16);
+        } else {
+            batch.draw(currentFrame, x, y, 16, 16);
+        }
     }
 
     public void dispose() {
         idleAnimation.getKeyFrames()[0].getTexture().dispose();
+        runLeftAnimation.getKeyFrames()[0].getTexture().dispose();
     }
 }
